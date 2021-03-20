7d560b6a519bb70c41fc16686c35d26edaaf7207
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2021-03-20 17:34:00 -0400

    Update time.c

--- time.c
+++ time.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 #define TIME_PRE "time"
-#define T_TIME 20201223
+static const char *TIME_NAME = "time";
 
 static time_t *YASLX_checktime(struct YASL_State *S, const char *name, int pos) {
-    if (!YASL_isuserdata(S, T_TIME)) {
+    if (!YASL_isuserdata(S, TIME_NAME)) {
         YASL_print_err(S, "TypeError: %s expected arg in position %d to be of type time, got arg of type %s.",
                 name, pos, YASL_peektypestr(S));
         YASL_throw_err(S, YASL_TYPE_ERROR);
@@ -27,7 +27,7 @@ static void YASL_pushtime(struct YASL_State *S, time_t time) {
     YASL_setmt(S);
 }
 
-static void YASL_time_tostr(struct YASL_State *S) {
+static int YASL_time_tostr(struct YASL_State *S) {
     char buff[26];
     time_t *time = YASLX_checktime(S, "time.tostr", 0);
     size_t len = strftime(buff, sizeof buff , "%FT%T%z", gmtime(time));
@@ -35,26 +35,27 @@ static void YASL_time_tostr(struct YASL_State *S) {
 
     char *buffer = malloc(strlen(buff) + 1);
     strcpy(buffer, buff);
-    YASL_pushszstring(S, buffer);
+    YASL_pushzstr(S, buffer);
+    return 1;
 }
 
-static void YASL_time_now(struct YASL_State *S) {
+static int YASL_time_now(struct YASL_State *S) {
     YASL_pushtime(S, time(NULL));
+    return 1;
 }
 
 void YASL_load_dyn_lib(struct YASL_State *S) {
     YASL_pushtable(S);
-    YASL_registermt(S, TIME_PRE);
+    YASL_registermt(S, TIME_NAME);
 
-    YASL_loadmt(S, TIME_PRE);
-    YASL_pushlitszstring(S, "tostr");
+    YASL_loadmt(S, TIME_NAME);
+    YASL_pushlit(S, "tostr");
     YASL_pushcfunction(S, YASL_time_tostr, 1);
     YASL_tableset(S);
 
     YASL_pushtable(S);
 
-    YASL_duptop(S);
-    YASL_pushlitszstring(S, "now");
+    YASL_pushlit(S, "now");
     YASL_pushcfunction(S, YASL_time_now, 0);
     YASL_tableset(S);
 }
