cb260e5e08692c49ec99f44b1c393f3f5f323fd4
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2022-11-05 10:52:22 -0400

    clean up

--- time.c
+++ time.c
@@ -18,8 +18,7 @@ struct YASL_Time {
 };
 
 struct YASL_TimeDelta {
-	long diff;
-	int milliseconds;
+	long long milliseconds;
 };
 
 static struct YASL_Time *YASLX_checktime(struct YASL_State *S, const char *name, int pos) {
@@ -48,7 +47,7 @@ static struct YASL_Time *allocate_time(time_t time, int milliseconds) {
 
 static struct YASL_TimeDelta *allocate_timedelta(long seconds, int milliseconds) {
 	struct YASL_TimeDelta *ptr = malloc(sizeof(struct YASL_TimeDelta));
-	*ptr = ((struct YASL_TimeDelta) {.diff = seconds, .milliseconds = milliseconds});
+	*ptr = ((struct YASL_TimeDelta) { .milliseconds = seconds * 1000 + milliseconds});
 	return ptr;
 }
 
@@ -68,15 +67,15 @@ static void YASL_pushtime(struct YASL_State *S, time_t time, int milliseconds) {
 	YASL_setmt(S);
 }
 
-static void YASL_pushtimedelta(struct YASL_State *S, long seconds, int milliseconds) {
-	YASL_pushuserdata(S, allocate_timedelta(seconds, milliseconds), TIME_DELTA_NAME, (void(*)(struct YASL_State *, void *))free_timedelta);
+static void YASL_pushtimedelta(struct YASL_State *S, int milliseconds) {
+	YASL_pushuserdata(S, allocate_timedelta(milliseconds / 1000, milliseconds % 1000), TIME_DELTA_NAME, (void(*)(struct YASL_State *, void *))free_timedelta);
 	YASL_loadmt(S, TIME_DELTA_NAME);
 	YASL_setmt(S);
 }
 
 static int YASL_time_tostr(struct YASL_State *S) {
 	char buff[30]; // 26 for the standard C stuff, 1 for decimal place, 3 more for milliseconds.
-	struct YASL_Time *time = YASLX_checktime(S, TIME_PRE "tostr", 0);
+	struct YASL_Time *time = YASLX_checktime(S, TIME_PRE ".tostr", 0);
 	size_t len = strftime(buff, sizeof buff, "%FT%T%z", gmtime(&time->time));
 	buff[len] = '\0';
 
@@ -148,8 +147,8 @@ int YASL_time_time(struct YASL_State *S) {
 }
 
 int YASL_time___sub(struct YASL_State *S) {
-	struct YASL_Time *right = YASLX_checktime(S, TIME_PRE "tostr", 1);
-	struct YASL_Time *left = YASLX_checktime(S, TIME_PRE "tostr", 0);
+	struct YASL_Time *right = YASLX_checktime(S, TIME_PRE ".__sub", 1);
+	struct YASL_Time *left = YASLX_checktime(S, TIME_PRE ".__sub", 0);
 
 	double diff = difftime(left->time, right->time);
 	int ms_diff = left->milliseconds - right->milliseconds;
@@ -162,39 +161,70 @@ int YASL_time___sub(struct YASL_State *S) {
 		diff++;
 	}
 
-	YASL_pushtimedelta(S, (long)diff, ms_diff);
+	YASL_pushtimedelta(S, (long)diff * 1000 + ms_diff);
 
 	return 1;
 }
 
 int YASL_timedelta_tostr(struct YASL_State *S) {
-	struct YASL_TimeDelta *td = YASLX_checktimedelta(S, TIME_DELTA_PRE "tostr", 1);
+	struct YASL_TimeDelta *td = YASLX_checktimedelta(S, TIME_DELTA_PRE ".tostr", 1);
 
-	char *buffer = NULL;
-	int len = snprintf(NULL, 0, "timedelta(%ld.%03d)", td->diff, td->milliseconds);
+    long long ms = td->milliseconds;
+    const char *sign = "";
+    if (ms < 0) {
+        sign = "-";
+        ms = -ms;
+    }
+
+    char *buffer = NULL;
+	int len = snprintf(NULL, 0, "timedelta(%s%lld.%03lld)", sign, ms / 1000, ms % 1000);
 	buffer = realloc(buffer, len + 1);
-	snprintf(buffer, len + 1, "timedelta(%ld.%03d)", td->diff, td->milliseconds);
+	snprintf(buffer, len + 1, "timedelta(%s%lld.%03lld)", sign, ms / 1000, ms % 1000);
 	buffer[len] = '\0';
 
 	YASL_pushzstr(S, buffer);
 	return 1;
 }
 
+int YASL_timedelta___neg(struct YASL_State *S) {
+	struct YASL_TimeDelta *td = YASLX_checktimedelta(S, TIME_DELTA_PRE ".__neg", 1);
+
+    YASL_pushtimedelta(S, -td->milliseconds);
+    return 1;
+}
+
+int YASL_timedelta___add(struct YASL_State *S) {
+	struct YASL_TimeDelta *left = YASLX_checktimedelta(S, TIME_DELTA_PRE ".__add", 0);
+	struct YASL_TimeDelta *right = YASLX_checktimedelta(S, TIME_DELTA_PRE ".__add", 1);
+
+    int total_ms = left->milliseconds + right->milliseconds;
+
+	YASL_pushtimedelta(S, total_ms);
+    return 1;
+}
+
+
+int YASL_timedelta___sub(struct YASL_State *S) {
+	struct YASL_TimeDelta *left = YASLX_checktimedelta(S, TIME_DELTA_PRE ".__sub", 0);
+	struct YASL_TimeDelta *right = YASLX_checktimedelta(S, TIME_DELTA_PRE ".__sub", 1);
+
+    int total_ms = right->milliseconds - left->milliseconds;
+
+	YASL_pushtimedelta(S, total_ms);
+    return 1;
+}
+
 int YASL_timedelta_frommilliseconds(struct YASL_State *S) {
 	yasl_int milliseconds = YASLX_checknint(S, TIME_PRE ".frommilliseconds", 0);
 
-	long seconds = milliseconds / 1000;
-	milliseconds %= 1000;
-
-	YASL_pushtimedelta(S, seconds, milliseconds);
-
+	YASL_pushtimedelta(S, milliseconds);
 	return 1;
 }
 
 int YASL_timedelta_fromseconds(struct YASL_State *S) {
 	yasl_int seconds = YASLX_checknint(S, TIME_PRE ".fromseconds", 0);
 
-	YASL_pushtimedelta(S, seconds, 0);
+	YASL_pushtimedelta(S, seconds * 1000);
 
 	return 1;
 }
@@ -203,7 +233,7 @@ int YASL_timedelta_fromminutes(struct YASL_State *S) {
 	yasl_int minutes = YASLX_checknint(S, TIME_PRE ".fromminutes", 0);
 
 	long seconds = minutes * 60;
-	YASL_pushtimedelta(S, seconds, 0);
+	YASL_pushtimedelta(S, seconds * 1000);
 
 	return 1;
 }
@@ -212,7 +242,7 @@ int YASL_timedelta_fromhours(struct YASL_State *S) {
 	yasl_int hours = YASLX_checknint(S, TIME_PRE ".fromhours", 0);
 
 	long seconds = hours * 60 * 60;
-	YASL_pushtimedelta(S, seconds, 0);
+	YASL_pushtimedelta(S, seconds * 1000);
 
 	return 1;
 }
@@ -234,6 +264,18 @@ void YASL_load_dyn_lib(struct YASL_State *S) {
 
 	YASL_pushlit(S, "tostr");
 	YASL_pushcfunction(S, YASL_timedelta_tostr, 1);
+	YASL_tableset(S);
+
+	YASL_pushlit(S, "__neg");
+	YASL_pushcfunction(S, YASL_timedelta___neg, 1);
+	YASL_tableset(S);
+
+	YASL_pushlit(S, "__add");
+	YASL_pushcfunction(S, YASL_timedelta___add, 2);
+	YASL_tableset(S);
+
+	YASL_pushlit(S, "__sub");
+	YASL_pushcfunction(S, YASL_timedelta___sub, 2);
 	YASL_tableset(S);
 
 	YASL_registermt(S, TIME_DELTA_NAME);
